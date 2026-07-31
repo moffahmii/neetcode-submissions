18e94b491fe258e914c96f983735fd420e0b0cfc
Mohamed Fahmy <fahmymohamed300@gmail.com>
2026-07-31 22:12:24 +0300

    Add: top-k-elements-in-list - submission-0

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-0.js
@@ -0,0 +1,36 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} k
+     * @return {number[]}
+     */
+    topKFrequent(nums, k) {
+
+        const freq = new Map();
+
+        for (const num of nums) {
+            freq.set(num, (freq.get(num) || 0) + 1);
+        }
+
+        const buckets = Array(nums.length + 1)
+            .fill(0)
+            .map(() => []);
+
+        for (const [num, count] of freq) {
+            buckets[count].push(num);
+        }
+
+        const result = [];
+
+        for (let i = buckets.length - 1; i >= 0 && result.length < k; i--) {
+            for (const num of buckets[i]) {
+                result.push(num);
+
+                if (result.length === k)
+                    break;
+            }
+        }
+
+        return result;
+    }
+}
